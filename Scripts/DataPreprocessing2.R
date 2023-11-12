49f84d398e3ec23a7dcb0ed624235eb97c938cf4
sudo-poweroff <sdellaporta34@gmail.com>
2023-11-12 18:45:23 +0100

    Modified files destination path

--- Scripts/DataPreprocessing2.R
+++ Scripts/DataPreprocessing2.R
@@ -23,6 +23,6 @@ names(sub_datasets)<-name_sub_dataset
 #Save the sub datasets
 lapply(names(sub_datasets), function(nome) {
   dataset <- sub_datasets[[nome]] 
-  file_name <- file.path("./Data/", paste0(nome, ".csv"))  
+  file_name <- file.path("./Data/SubDatasets", paste0(nome, ".csv"))  
   write.csv(dataset, file = file_name, row.names = FALSE)
 })
