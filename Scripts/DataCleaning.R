cfcca3e1b476c54e3d26a2e57907c1673c0a74f1
Rocco000 <r.iuliano13@studenti.unisa.it>
2023-11-22 11:00:07 +0100

    Extracted period 2010-2021, renamed script files

--- Scripts/DataCleaning.R
+++ Scripts/DataCleaning.R
@@ -1,9 +1,11 @@
-# Delete irrelevant features
+# Delete irrelevant features and select period 2010-2021
 selected_features <- c("Country","Commodity","Indicator","Measure","Time","Unit","PowerCode","Value")
 
 cleaned_data <- subset(OriginData, select = selected_features)
 
 columns <- names(cleaned_data)
 print(columns)
+
+cleaned_data <- subset(cleaned_data, Time %in% c(2010,2021))
 
 write.csv(cleaned_data, "./Data/CleanedData.csv", row.names = FALSE)
