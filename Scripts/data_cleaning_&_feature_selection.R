6fe794d7e262287d83bfaf2bef404478579902ba
sudo-poweroff <sdellaporta34@gmail.com>
2024-08-07 18:45:40 +0200

    Changed nomenclature

--- Scripts/data_cleaning_&_feature_selection.R
+++ Scripts/data_cleaning_&_feature_selection.R
@@ -2,7 +2,7 @@ data_cleaning <- function(){
   # Delete irrelevant features
   selected_features <- c("Country","Commodity","Indicator","Measure","Time","Unit","PowerCode","Value")
   
-  cleaned_data <- subset(OriginData, select = selected_features)
+  cleaned_data <- subset(origin_data, select = selected_features)
   
   columns <- names(cleaned_data)
   print(columns)
@@ -10,19 +10,20 @@ data_cleaning <- function(){
   #Extract unit of interest
   cleaned_data<- subset(cleaned_data, Measure %in% c("Tonnes","Euro","Percentage","Ratio"))
   
-  write.csv(cleaned_data, "./Data/CleanedData.csv", row.names = FALSE)
+  write.csv(cleaned_data, "./Data/cleaned_dataset.csv", row.names = FALSE)
 }
+
+data_cleaning()
 
 feature_selection <- function(){
   #Extract years of interest
-  final_data <- subset(CleanedData, Time %in% 2005:2020)
+  final_data <- subset(cleaned_dataset, Time %in% 2005:2020)
   
   #Extract indicators of interest
   final_data <-  subset(final_data, Indicator %in% c("Level of production","Level of consumption (at farm gate)","Consumption price","Producer price (at farm gate)","Market Price Differential"))
   
   #Save the csv file
-  write.csv(final_data, "./Data/FinalDataset.csv", row.names = FALSE)
+  write.csv(final_data, "./Data/final_dataset.csv", row.names = FALSE)
 }
 
-data_cleaning()
 feature_selection()
--- Scripts/data_splitting.R
+++ Scripts/data_splitting.R
@@ -1,17 +1,17 @@
 # Split dataset based on selected Indicators
-sub_datasets <- split(FinalDataset, FinalDataset$Indicator)
+sub_datasets <- split(final_dataset, final_dataset$Indicator)
 
 names(sub_datasets)
 
-name_sub_dataset <- c("ConsumptionPrice","LevelOfConsumption_AtFarmGate",
-                      "LevelOfProduction","MarketPriceDifferential","ProducerPrice_AtFarmGate"
+name_sub_dataset <- c("consumption_price","level_of_consumption_at_farm_gate",
+                      "level_of_production","market_price_differential","producer_price_at_farm_gate"
                       )
 
 names(sub_datasets)<-name_sub_dataset
 
 #Save the sub datasets
-lapply(names(sub_datasets), function(nome) {
-  dataset <- sub_datasets[[nome]] 
-  file_name <- file.path("./Data/SubDatasets", paste0(nome, ".csv"))  
+lapply(names(sub_datasets), function(name) {
+  dataset <- sub_datasets[[name]] 
+  file_name <- file.path("./Data/SubDatasets", paste0(name, ".csv"))  
   write.csv(dataset, file = file_name, row.names = FALSE)
 })
