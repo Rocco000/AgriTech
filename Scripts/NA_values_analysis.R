d54345941bfeac4f8e258936fca4369195c84d86
sudo-poweroff <sdellaporta34@gmail.com>
2024-08-07 19:08:29 +0200

    Added code to save the plot

--- Scripts/NA_values_analysis.R
+++ Scripts/NA_values_analysis.R
@@ -3,7 +3,7 @@ na_counter_per_year <- function(subdataset){
   na_count_per_year_indicator <- aggregate(Value ~ Time + Indicator, data = subdataset, function(x) sum(is.na(x)))
 }
 
-na_values <- na_counter_per_year(CleanedData)
+na_values <- na_counter_per_year(cleaned_dataset)
 na_values <- subset(na_values, Indicator %in% c("Level of production","Level of consumption (at farm gate)","Consumption price","Producer price (at farm gate)","Market Price Differential"))
 
 # Load ggplot2
@@ -13,7 +13,9 @@ library(ggplot2)
 ggplot(na_values, aes(x = Time, y = Value, color = Indicator, group = Indicator)) +
   geom_line() + 
   geom_point() +
-  labs(title = "Conteggio dei valori NA per Anno e Indicatore",
+  labs(title = "Conteggio dei valori NA",
        x = "Anno",
        y = "Numero di Valori NA") +
   theme_minimal()
+
+ggsave("./Data/DataExploration/NA_values_analysis.png")
