4497bc68f34549e48b7079df703bf3c6c51c5ca0
Rocco000 <r.iuliano13@studenti.unisa.it>
2024-08-08 18:34:25 +0200

    Filtered dataset on commodity column

--- Scripts/data_cleaning_&_feature_selection.R
+++ Scripts/data_cleaning_&_feature_selection.R
@@ -1,4 +1,6 @@
 data_cleaning <- function(){
+  origin_data <- read.csv("Data/origin_data.csv")
+  
   # Delete irrelevant features
   selected_features <- c("Country","Commodity","Indicator","Measure","Time","Unit","PowerCode","Value")
   
@@ -16,11 +18,16 @@ data_cleaning <- function(){
 data_cleaning()
 
 feature_selection <- function(){
-  #Extract years of interest
+  cleaned_dataset <- read.csv("Data/cleaned_dataset.csv")
+  
+  # Extract years of interest
   final_data <- subset(cleaned_dataset, Time %in% 2005:2020)
   
-  #Extract indicators of interest
+  # Extract indicators of interest
   final_data <-  subset(final_data, Indicator %in% c("Level of production","Level of consumption (at farm gate)","Consumption price","Producer price (at farm gate)","Market Price Differential"))
+  
+  # Extract commodities of interest
+  final_data <- subset(final_data, Commodity %in% c("Wheat", "Soybeans", "Maize", "Rice", "Sugar", "Oats", "Beef and veal", "Coffee"))
   
   #Save the csv file
   write.csv(final_data, "./Data/final_dataset.csv", row.names = FALSE)
