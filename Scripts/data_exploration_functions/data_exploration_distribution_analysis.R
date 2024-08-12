3bf199749ca0567558dff137b230b23cfb64aefd
Rocco000 <r.iuliano13@studenti.unisa.it>
2024-08-12 18:45:40 +0200

    Developed code to distribution analysis (per indicator)

--- Scripts/data_exploration_functions/data_exploration_distribution_analysis.R
+++ Scripts/data_exploration_functions/data_exploration_distribution_analysis.R
@@ -10,14 +10,14 @@ box_plot_per_indicator_commodity <- function(ds, commodity, indicator){
   
   ggplot(ds, aes(x = Value, y = Country, fill = Country)) +
     geom_boxplot() +
-    labs(title = paste("Box Plot of",commodity," Indicators by Country"),
+    labs(title = paste0("Box Plot of Indicator ",indicator," by Country for ", commodity),
          x = "Value",
          y = "Country") +
     theme_minimal() +
     theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
     scale_x_continuous(labels = scales::comma)
   
-  save_path = str_replace_all(paste("Data/DataExploration/Summary/",indicator,"/boxplot_",commodity,".png"), " ", "")
+  save_path = paste0("Data/DataExploration/Summary/",indicator,"/boxplot_",commodity,".png")
   ggsave(save_path, width = 10, height = 5)
 }
 
@@ -25,7 +25,7 @@ box_plot_per_indicator_commodity <- function(ds, commodity, indicator){
 summary_per_country_commodity <- function(ds, indicator){
   
   # Get only the tuples with indicator parameter value as Indicator
-  ds <- subset(ds, Indicator == indicator)
+  ds <- base::subset(ds, Indicator == indicator)
   
   # Get all countries and commodities
   countries <- get_countries(ds)
@@ -39,7 +39,9 @@ summary_per_country_commodity <- function(ds, indicator){
     Median = numeric(0),
     Mean = numeric(0),
     Third_quartile = numeric(0),
-    Max = numeric(0)
+    Max = numeric(0),
+    Variance = numeric(0),
+    Std = numeric(0)
   )
   
   # Execute the summary function for each commodity per country
@@ -47,8 +49,14 @@ summary_per_country_commodity <- function(ds, indicator){
     
     for(country in countries){
       
-      filtered_ds <- subset(ds, Commodity == commodity & Country == country)
-      x <- summary(filtered_ds$Value)
+      # Filter the dataset by commodity and country
+      filtered_ds <- base::subset(ds, Commodity == commodity & Country == country)
+      
+      # Calculate the summary statistics
+      x <- base::summary(filtered_ds$Value)
+      variance <- var(filtered_ds$Value)
+      std <- sd(filtered_ds$Value)
+      
       df <- data.frame(
         Country = c(country),
         Commodity = c(commodity),
@@ -57,13 +65,15 @@ summary_per_country_commodity <- function(ds, indicator){
         Median = c(x[3]),
         Mean = c(x[4]),
         Third_quartile = c(x[5]),
-        Max = c(x[6])
+        Max = c(x[6]),
+        Variance = c(variance),
+        Std = c(std)
       )
       
       if (nrow(to_csv) == 0) {
         to_csv <- df
       } else {
-        to_csv <- rbind(to_csv, df)
+        to_csv <- base::rbind(to_csv, df)
       }
     }
     
@@ -72,7 +82,7 @@ summary_per_country_commodity <- function(ds, indicator){
   # Create the folder path if doesn't exist
   indicator <- str_trim(indicator)
   indicator <- str_replace_all(indicator, " ", "_")
-  folder_path <- str_replace_all(paste("Data/DataExploration/Summary/",indicator), " ", "")
+  folder_path <- paste0("Data/DataExploration/Summary/",indicator)
   
   if (!dir.exists(folder_path)) {
     dir.create(folder_path, recursive = TRUE)
@@ -80,14 +90,82 @@ summary_per_country_commodity <- function(ds, indicator){
   }
   
   # Store the summary results in a CSV file
-  save_path <- str_replace_all(paste(folder_path,"/summary.csv"), " ", "")
+  save_path <- paste0(folder_path,"/summary_per_country_commodity.csv")
   write.csv(to_csv, file = save_path, row.names = FALSE)
   
   # Create the box plot
   for(commodity in commodities){
-    filtered_ds <- subset(ds, Commodity == commodity)
+    filtered_ds <- base::subset(ds, Commodity == commodity)
     box_plot_per_indicator_commodity(filtered_ds, commodity, indicator)
   }
+  
+}
+
+# Get the summary statistics per indicator and commodity
+summary_per_indicator_commodity <- function(ds, indicator){
+  
+  # Get only the tuples with indicator parameter value as Indicator
+  ds <- base::subset(ds, Indicator == indicator)
+  
+  # Get all commodities
+  commodities <- get_commodities(ds)
+  
+  to_csv <- data.frame(
+    Commodity = character(0),
+    Min = numeric(0),
+    First_quartile = numeric(0),
+    Median = numeric(0),
+    Mean = numeric(0),
+    Third_quartile = numeric(0),
+    Max = numeric(0),
+    Variance = numeric(0),
+    Std = numeric(0)
+  )
+  
+  # Execute the summary function for each commodity per country
+  for(commodity in commodities){
+    
+    # Filter the dataset by commodity and country
+    filtered_ds <- base::subset(ds, Commodity == commodity)
+    
+    # Calculate the summary statistics
+    x <- base::summary(filtered_ds$Value)
+    variance <- var(filtered_ds$Value)
+    std <- sd(filtered_ds$Value)
+    
+    df <- data.frame(
+      Commodity = c(commodity),
+      Min = c(x[1]),
+      First_quartile = c(x[2]),
+      Median = c(x[3]),
+      Mean = c(x[4]),
+      Third_quartile = c(x[5]),
+      Max = c(x[6]),
+      Variance = c(variance),
+      Std = c(std)
+    )
+    
+    if (nrow(to_csv) == 0) {
+      to_csv <- df
+    } else {
+      to_csv <- base::rbind(to_csv, df)
+    }
+    
+  }
+  
+  # Create the folder path if doesn't exist
+  indicator <- str_trim(indicator)
+  indicator <- str_replace_all(indicator, " ", "_")
+  folder_path <- paste0("Data/DataExploration/Summary/",indicator)
+  
+  if (!dir.exists(folder_path)) {
+    dir.create(folder_path, recursive = TRUE)
+    cat("Folder created:", folder_path, "\n")
+  }
+  
+  # Store the summary results in a CSV file
+  save_path <- paste0(folder_path,"/summary.csv")
+  write.csv(to_csv, file = save_path, row.names = FALSE)  
   
 }
 
@@ -97,5 +175,6 @@ main_distribution_analysis <- function(ds){
   
   for(indicator in indicators){
     summary_per_country_commodity(ds, indicator)
+    summary_per_indicator_commodity(ds, indicator)
   }
 }
--- Scripts/data_exploration_functions/main.R
+++ Scripts/data_exploration_functions/main.R
@@ -5,6 +5,6 @@ source("Scripts/data_exploration_functions/data_exploration_distribution_analysi
 agricultural_dataset <- read.csv("Data/final_dataset.csv")
 #agricultural_dataset$Country <- factor(agricultural_dataset$Country)
 
-main_geographical_analysis(agricultural_dataset)
+main_geographical_analysis(read.csv("Data/cleaned_dataset.csv"))
 
 main_distribution_analysis(agricultural_dataset)
